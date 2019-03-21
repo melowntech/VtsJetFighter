56946571f7a37bacef20c1fa776f10a297f3e461
Tomáš Malý <tomas.maly@melown.com>
2019-03-21 15:30:53 +0100

    fix crash with reset

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityStandardAssets.Vehicles.Aeroplane;
 
 public class Controls : MonoBehaviour
 {
     private AeroplaneController aircraft;
-    private bool stopViewToggle = false;
 
     public float mouseSensitivity = 3;
     public float throttle = 1;
-
+    
     void Start()
     {
         aircraft = GetComponent<AeroplaneController>();
@@ -41,21 +39,6 @@ public class Controls : MonoBehaviour
             roll = Input.GetAxis("Horizontal");
             pitch = Input.GetAxis("Vertical");
         }
-        if (Input.GetAxis("Reset") > 0.0)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        if (Input.GetAxis("StopView") > 0.0)
-        {
-            if (!stopViewToggle)
-            {
-                stopViewToggle = true;
-                aircraft.enabled = !aircraft.enabled;
-                GetComponent<Rigidbody>().isKinematic = !aircraft.enabled;
-            }
-        }
-        else
-            stopViewToggle = false;
         aircraft.Move(roll, pitch, 0, throttle, false);
     }
 }
